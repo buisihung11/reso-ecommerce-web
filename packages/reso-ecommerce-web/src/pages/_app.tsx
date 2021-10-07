1494513d5dff73d05c06fdc4348c47445c719db7
HungHCC <buisihung321@gmail.com>
2021-10-07 09:47:00 +0700

    type(style) : Add np progress

--- packages/reso-ecommerce-web/src/pages/_app.tsx
+++ packages/reso-ecommerce-web/src/pages/_app.tsx
@@ -1,27 +1,27 @@
-import { AppProps } from 'next/app';
+import ThemePrimaryColor from '@/components/ThemePrimaryColor';
+import { StoreProvider } from '@/contexts/store-context';
+import '@/styles/global.css';
+import '@/styles/reset.css';
+import '@/styles/variables.css';
 // theme
 import ThemeConfig from '@/theme';
 import GlobalStyles from '@/theme/globalStyles';
-import ThemePrimaryColor from '@/components/ThemePrimaryColor';
-
-
+import theme from '@/theme/theme';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { AppProps } from 'next/app';
+import NextNprogress from 'nextjs-progressbar';
 import { FC, useState } from 'react';
-import { StoreProvider } from '@/contexts/store-context';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import createEmotionCache from '../components/createEmotionCache';
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-import { CacheProvider, EmotionCache } from '@emotion/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
 import 'simplebar/dist/simplebar.min.css';
-import '@/styles/reset.css';
-import '@/styles/variables.css';
-import '@/styles/global.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import createEmotionCache from '../components/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 const Noop: FC = ({ children }) => <>{children}</>;
 interface MyAppProps extends AppProps {
@@ -40,6 +40,13 @@ export default function MyApp({
     <CacheProvider value={emotionCache}>
       <ThemeConfig>
         <ThemePrimaryColor>
+          <NextNprogress
+            color={theme.palette.primary.main}
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={3}
+            showOnShallow={true}
+          />
           <GlobalStyles />
           <ToastContainer />
           <QueryClientProvider client={queryClient}>
